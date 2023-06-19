0f3ab3379af9c93230dcae220d6c11cc61e61036
DaniWeos <danielweoswork@gmail.com>
2023-06-19 19:51:02 -0300

    Commit Streams

---
+++ DartAssincronismo/bin/timing/waiting_time.dart
@@ -0,0 +1,13 @@
+class BotBlock {
+  Future clock(int seconds) {
+    return Future.delayed(Duration(seconds: seconds));
+  }
+  Stream kakoBotStream(int interval, [int? maxCount]) async* {
+    int i = 1;
+    while (i != maxCount) {
+      await Future.delayed(Duration(seconds: interval));
+      yield i++;
+    }
+    print("Stream acabou");
+  }
+}
--- DartExceptions/main_curso04.dart
+++ DartExceptions/main_curso04.dart
@@ -1,8 +1,45 @@
+import 'dart:math';
+
 import 'Exceptions/bank_controler_exceptions.dart';
 import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 
+void testingNullSafety() {
+  Account? myAccount = Account(name: "Daniel", balance: 200, isAuthenticated: true);
+
+  //Simulando comunicação externa
+  Random rng = Random();
+  int randomNumber = rng.nextInt(10);
+  if (randomNumber <= 5) {
+    myAccount.createdAt = DateTime.now();
+  }
+  print(myAccount.runtimeType);
+
+
+  print(myAccount.createdAt);
+  //print(myAccount.createdAt.day);
+  // //print(myAccount!.balance);
+  // //print(myAccount.balance);
+  print(myAccount.createdAt!.day);
+
+
+  if (myAccount != null) {
+    print(myAccount.balance);
+    if(myAccount.createdAt != null){
+      print(myAccount.createdAt!.day);
+    }
+  } else {
+    print("Conta nula");
+  }
+  print(myAccount != null ? myAccount.balance : "Conta nula");
+
+  //Chamada segura
+  print(myAccount.balance);
+}
+
 void main() {
+  testingNullSafety();
+
   // Criando o banco
   BankController bankController = BankController();
 
@@ -23,7 +60,7 @@ void main() {
         idSender: "Kako", idReceiver: "Ricarth", amount: 50);
     // Observando resultado
     if (result) {
-      print("Transação concluida com sucesso");
+      //print("Transação concluida com sucesso");
     }
   } on SenderIdInvalidException catch (e) {
     print(e);
